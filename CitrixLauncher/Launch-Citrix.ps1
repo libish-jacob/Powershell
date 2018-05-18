c48bfb65ac010d0241948f7e34786ee962bcbe71
Libish Jacob <jajac@vestas.com>
2018-05-18 09:07:24 +0530

    Citrix launcher Powershell script checked in
    Citrix launcher Powershell script checked in

---
+++ CitrixLauncher/Launch-Citrix.ps1
@@ -0,0 +1,72 @@
+#$ErrorActionPreference = "silentlyContinue"
+
+function GetLoginItemName([psobject] $ie, [string] $tagName) {
+    $inputs = $ie.Document.getElementsByTagName("input")
+    $loginItemName = ($inputs | Where-Object {$_.name -eq $tagName})
+    return $loginItemName
+}
+
+function GetLoginItemId([psobject] $ie, [string] $tagName) {
+    $inputs = $ie.Document.getElementsByTagName("input")
+    $loginItemName = ($inputs | Where-Object {$_.id -eq $tagName})
+    return $loginItemName
+}
+
+function GetVDIItemLink([psobject] $ie) {
+    $inputs = $ie.Document.getElementsByTagName("div")
+    #$link = ($inputs | Where-Object {$_.id -eq "desktopSpinner_idCitrix.MPS.Desktop.XD75.XD_0020Windows_0020Dedicated_0020_0024P2034"})
+    $link = ($inputs | Where-Object {$_.id -Like "desktopSpinner_idCitrix.MPS.Desktop.XD75.XD_0020Windows_0020Dedicated_*"})                                                          
+    return $link
+}
+
+#$Item = @("hel","hell","hello")
+#$items = $Item | Where-Object {$_ -like "he*"}
+#$items.Count
+
+
+$ie = new-object -com internetexplorer.application
+$ie.visible = $false
+$ie.ParsedHtml
+$ie.navigate2("citrix")
+
+"Launching Please wait..."
+while ($ie.busy) {Start-Sleep 2}
+
+$loginItemName = GetLoginItemName $ie "login" 
+$link = GetVDIItemLink($ie)
+
+while (!$loginItemName -and !$link) {
+    Start-Sleep 2
+    $loginItemName = GetLoginItemName $ie "login"
+    $link = GetVDIItemLink($ie)
+}
+
+if ($loginItemName) {
+    $username = $env:UserName
+    $SecurePassword = Read-Host -AsSecureString -Prompt "Provide password for $username"
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
+    $Password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    
+    $loginItemName = GetLoginItemName $ie "login"
+    if ($loginItemName) {
+        "Logging in..."
+        $loginItemName.value = "jajac"
+        $loginPasswordItem = GetLoginItemName $ie "passwd" 
+        $loginPasswordItem.value = $Password
+
+        $logonLink = GetLoginItemId $ie "Log_On"
+        $logonLink.click()
+    }
+}
+
+while (!$link) {
+    $link = GetVDIItemLink($ie)
+    Start-Sleep 3
+}
+
+"Starting VDI..."
+
+$link.click()
+
+#wait for some time before exit so that user can see the activity log.
+Start-Sleep -s 5
---
+++ CitrixLauncher/Readme.txt
@@ -0,0 +1,10 @@
+This is a powershell script to launch browser login using domain account name. Password will be asked while it runs.
+This will launch the citrix VDI VM.
+
+This script automates the process of
+1. Launch IE
+2. Type url
+3. Login if not already logged in
+4. Click on VDI to login.
+
+Note: For This script to execute properly we have to have the protected mode in IE to be set to off.
